5c4e841b38cf79158b86f5179ea5a046d1fd258b
iibrahimli <imranibrahimli98@gmail.com>
2019-11-21 02:06:40 +0400

    add metrics

--- mlp/metrics.py
+++ mlp/metrics.py
@@ -8,6 +8,25 @@ Performance metrics.
 import numpy as np
 
 
+def tfpn(y_true, y_pred):
+    """
+    Helper function to calculate TP, TN, FP, FN
+
+    Args:
+        y_true (np.ndarray): Ground truth value
+        y_pred (np.ndarray): Predicted value
+    
+    Returns:
+        tp, tn, fp, fn:      Values
+    """
+
+    tp = np.sum((y_true == 1) & (y_pred == 1))
+    tn = np.sum((y_true == 0) & (y_pred == 0))
+    fp = np.sum((y_true == 0) & (y_pred == 1))
+    fn = np.sum((y_true == 1) & (y_pred == 0))
+    return tp, tn, fp, fn
+
+
 class metric:
     """
     Abstract class of a metric function that returns a scalar given predicted
@@ -36,24 +55,46 @@ class accuracy(metric):
 
 class precision(metric):
     def __call__(self, y_true, y_pred):
-        pass
+        tp, tn, fp, fn = tfpn(y_true, y_pred)
+        return tp / (tp + fp)
 
 
 class sensitivity(metric):
     def __call__(self, y_true, y_pred):
-        pass
+        tp, tn, fp, fn = tfpn(y_true, y_pred)
+        return tp / (tp + fn)
 
 
-class sensitivity(metric):
+class specificity(metric):
     def __call__(self, y_true, y_pred):
-        pass
+        tp, tn, fp, fn = tfpn(y_true, y_pred)
+        return tn / (tn + fp)
 
 
 class f1(metric):
     def __call__(self, y_true, y_pred):
-        pass
+        tp, tn, fp, fn = tfpn(y_true, y_pred)
+        return 2 * tp / (2 * tp + fp + fn)
 
 
 class kappa(metric):
     def __call__(self, y_true, y_pred):
         pass
+
+
+def k_fold_cv(net, k, x, y):
+    """
+    K-fold cross-validation
+
+    Args:
+        net:   The network to test
+        k:     K (default: 5)
+        x, y:  The dataset
+    
+    Returns:
+        res:   Dict of metrics (averaged)
+
+    """
+
+    # TODO
+    pass
